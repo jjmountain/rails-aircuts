ad6809837b8ffd3e962676e050cd7dfe16a7d668
Michael Frantz <mjfrantz26@gmail.com>
2019-02-26 21:49:42 +0900

    got the customer user story done up to the form

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,10 +1,15 @@
 class ShopsController < ApplicationController
+  after_action :verify_authorized, except: [:index, :show]
 
   def index
     @shops = policy_scope(Shop)
+    @shops = Shop.all
   end
 
   def show
-    @shop = shop.find(params[:id])
+    @shop = Shop.find(params[:id])
+    @user = current_user
+
+    @appointment = Appointment.new
   end
 end
