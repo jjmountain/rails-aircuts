39c687a1787a985747d544b3de0edf83270ee728
James Devereux <devereuxjj@gmail.com>
2019-02-28 14:31:52 +0900

    new seeds working from yelp api call, updated shop model, changed shops index page inline with new schema

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -5,11 +5,10 @@ class Shop < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :barbers, dependent: :destroy
   has_many :appointments, through: :barbers
-  HOURS = Array(0..23)
   validates :shop_name, presence: true
   validates :owner_name, presence: true
-  validates :description, presence: true
-  validates :address, presence: true
-  validates :open_at, presence: true, inclusion: { in: HOURS }
-  validates :close_at, presence: true, inclusion: { in: HOURS }
+  validates :longitude, presence: true
+  validates :latitude, presence: true
+  validates :longitude, presence: true
+  validates :yelp_id, presence: true
 end
